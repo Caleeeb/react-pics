9de53bc726c4009f357f349d890bf914aad420fc
Caleb Day <day.caleb@gmail.com>
2023-02-15 13:41:51 -0500

    api fetch handler start and is working

--- src/api.js
+++ src/api.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
-const searchImages = () => {
-  axios.get('http://api.unsplash.com/search/photos')
+const searchImages = async () => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
+    headers: {
+      Authorization: 'Client-ID v5S1xB4vPVTYOzfYPkfahL0jA7DsZ0LbTDYNuzcrcgU',
+    },
+    params: {
+      query: 'cars',
+    },
+  });
+  console.log(response);
+  return response;
 };
+
+export default searchImages;
