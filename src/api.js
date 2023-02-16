6ecf979885f245600b0f810eec79fe9ed339ab5c
Caleb Day <day.caleb@gmail.com>
2023-02-16 13:03:49 -0500

    api fetch function complete

--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
       Authorization: 'Client-ID v5S1xB4vPVTYOzfYPkfahL0jA7DsZ0LbTDYNuzcrcgU',
     },
     params: {
-      query: 'cars',
+      query: term,
     },
   });
-  console.log(response);
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
